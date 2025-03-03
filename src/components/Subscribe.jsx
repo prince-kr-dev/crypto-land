081d389b152f528a991e032ba405e46423d8b7ff
Prince Kumar <princekumark2004@gmail.com>
2025-03-04 01:05:41 +0530

    Update Subscribe.jsx

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -5,7 +5,7 @@ function Subscribe(){
             <div className="mt-5 md:mt-0 w-70 flex flex-col items-center md:items-start">
                 <h1 className="text-3xl font-medium text-white">Subscribe</h1>
                 <p className="text-white mt-2 font-medium text-center sm:text-left">Subscribe to our newsletter for the latest updates</p>
-                <input type="email" placeholder="Enter Email" className="bg-blue-300 px-3 py-2 rounded-xl font-medium text-xl mt-4"/>
+                <input type="email" placeholder="Enter Email" className="bg-blue-300 px-3 py-2 rounded-xl font-medium text-md w-60 mt-4"/>
             </div>
         </>
     );
